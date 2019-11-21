a2d679290e4b4afdc393d9f6be3aae94eef78c76
sobiyamoulvi <sobiyamoulvi299@gmail.com>
2019-11-21 13:38:04 +0530

    Added sampleAdd.py Addition of Two Numbers Version 2

--- sampleAdd.py
+++ sampleAdd.py
@@ -1,4 +1,5 @@
 print("Addition of  Two numbers")
 a=int(input("Enter 1st number:"))
 b=int(input("Enter 2nd number:"))
-print(a+b)
+print("Addition of ",a," and ",b," is :",a+b)
+print("Successfull!!")
